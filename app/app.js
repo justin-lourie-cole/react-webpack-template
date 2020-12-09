328646819da6c26603080853d8c34150baf90b3d
Justin Cole <justincole@outlook.co.nz>
2020-12-10 12:46:14 +1300

    initial commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState, useRef } from "react";
+import ReactDOM from "react-dom";
+import "./styles/main.scss";
+
+function App() {
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+if (module.hot) {
+	module.hot.accept();
+}
